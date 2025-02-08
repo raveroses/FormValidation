fef3064fe6514053091b7837cb7085d394fbf155
rave-roses <odekunlewaris01@gmail.com>
2025-02-09 00:12:29 +0100

    added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <p className="color-red-500">guy</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div>
+      <div>English</div>
+      <div className="">Login</div>
+    </div>
+  );
+}
