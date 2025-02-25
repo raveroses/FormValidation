56ba9ec25864e7fd5e2a741c5787c8819276b4eb
rave-roses <odekunlewaris01@gmail.com>
2025-02-25 23:30:41 +0100

    work in progress

--- src/Components/CardSection.jsx
+++ src/Components/CardSection.jsx
@@ -4,12 +4,21 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 // import useFetch from "./api/useFetch";
 // import { useEffect } from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 export default function CardSection() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const MovieFetch = async () => {
     setLoading(true);
-    console.log("yes");
+
     try {
       const response = await fetch(
         // "https://api.themoviedb.org/3/collection/10/images?api_key=b23cab54b01ec0634aae0d6fc905411b"
@@ -35,25 +44,29 @@ export default function CardSection() {
   const check = data.flatMap((item) => {
     return item.results.map((items, index) => {
       console.log(items.poster_path, index);
+      const dates = new Date(items.release_date);
+      const getYear = dates.getFullYear();
+      const rate = items.vote_average;
+      const shortenRate = rate.toFixed(1);
 
       return (
-        <div className="relative" key={index}>
+        <SwiperSlide key={index} className="relative">
           <img
             src={`https://image.tmdb.org/t/p/original/${items.poster_path}`}
             alt="card-movie-image"
-            className="w-[160px] opacity-[0.5] rounded-xl group-hover:opacity-[0.2] group-hover:w-[170px] group-hover:delay-100 group-hover:duration-500 group-hover:transition-all"
+            className="w-[180px] opacity-[0.5] rounded-xl group-hover:opacity-[0.2] group-hover:w-[170px] group-hover:delay-100 group-hover:duration-500 group-hover:transition-all"
           />
           <MdPlayCircle className="text-blue-500 absolute top-20 left-15 text-[40px] group-hover:block md:hidden" />
           <div className=" absolute left-0 top-40 px-3">
             <p className="text-white  text-[16px] font-bold-semibold">
-              Captain America: Brave New World
+              {items?.title}
             </p>
             <div className="flex items-center justify-between text-gray-200">
               <div className="flex items-center gap-2 font-medium">
-                <p className="text-[13px]">2025</p>
+                <p className="text-[13px]">{getYear}</p>
                 <span className="flex items-center">
                   <TiStarFullOutline className="text-[13px]" />
-                  <p className="text-[12px]"> 6.2</p>
+                  <p className="text-[12px]"> {shortenRate}</p>
                 </span>
               </div>
               <p className="text-[13px] border-1 px-1 border-gray-400 rounded">
@@ -62,7 +75,7 @@ export default function CardSection() {
               </p>
             </div>
           </div>
-        </div>
+        </SwiperSlide>
       );
     });
   });
@@ -74,9 +87,22 @@ export default function CardSection() {
         <h2 className="text-white text-[25px] border-b-1 border-white  w-[150px]">
           Top Trending
         </h2>
-        <div className="group card-div flex justify-around gap-4 w-full pt-7 ">
+        {/* <div className="group card-div flex justify-around gap-4 w-full pt-7 ">
           {check}
-        </div>
+        </div> */}
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={0}
+          slidesPerView={6}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          {check}
+        </Swiper>
       </section>
       <section className="top-rating"></section>
       <section className="popular"></section>
