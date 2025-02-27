1b19a944006ae56a7391eef963e02971b77c5218
rave-roses <odekunlewaris01@gmail.com>
2025-02-27 23:12:50 +0100

    ongoing

--- src/Page.jsx/Dashboard.jsx
+++ src/Page.jsx/Dashboard.jsx
@@ -2,7 +2,7 @@ import DashboardHeader from "../Components/DashBoardHeader";
 import CardSection from "../Components/CardSection";
 export default function Dashboard() {
   return (
-    <div className="bg-black">
+    <div className="bg-black pb-30">
       <DashboardHeader />
       <CardSection />
     </div>
