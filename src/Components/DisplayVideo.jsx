5dfbbe68053612b45c5eea0a62b7e6af565d8e7f
rave-roses <odekunlewaris01@gmail.com>
2025-03-05 23:59:31 +0100

    video display page setting up

--- src/Components/DisplayVideo.jsx
+++ src/Components/DisplayVideo.jsx
@@ -2,7 +2,9 @@ import { useParams } from "react-router-dom";
 import useFetch from "./api/UseFetch";
 import { useEffect, useContext } from "react";
 import UserContext from "../Context.jsx/UserContext";
-
+import { PiClockCountdown } from "react-icons/pi";
+import { BsStarFill } from "react-icons/bs";
+import { RiPlayFill } from "react-icons/ri";
 export default function DisplayVideo() {
   const { endpointChanger } = useContext(UserContext);
   let param = useParams();
@@ -30,25 +32,67 @@ export default function DisplayVideo() {
   // https://api.themoviedb.org/3/tv/63770?api_key=b23cab54b01ec0634aae0d6fc905411b
 
   return (
-    <>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, totam
-      doloribus impedit nemo sapiente facere vitae sit harum maxime nulla earum
-      repudiandae soluta eum porro explicabo nisi ab pariatur quibusdam. Nemo a
-      harum blanditiis id unde corporis facilis quod vitae accusamus natus
-      exercitationem illo fuga nostrum inventore est esse vero, perspiciatis ut
-      quidem nulla vel distinctio ducimus labore? Voluptate, obcaecati! Nisi rem
-      hic sint facilis dicta molestiae neque cum nulla in veritatis, mollitia
-      corporis maiores unde eaque quasi minus iste culpa fugit qui! Eius nulla
-      dolorem quidem, autem debitis similique! Accusantium quas maxime in animi,
-      atque, laboriosam nostrum maiores nesciunt neque autem eum debitis nihil
-      explicabo dolores non quia et ad iste reprehenderit aliquam dicta!
-      Officiis est ipsa quo perferendis. Nulla culpa explicabo quia recusandae
-      eveniet? Tenetur illum labore accusantium dolorum ipsa quasi adipisci
-      voluptatum nulla corporis esse cumque, sit ipsam. Qui odio dicta eligendi
-      reiciendis sed veniam dolor nihil? Perferendis, dignissimos! Nemo facere
-      culpa fugit fuga minus reprehenderit recusandae debitis sint impedit nulla
-      aperiam provident ipsa aliquam voluptate odio mollitia nobis nostrum,
-      veritatis illum voluptatem temporibus commodi! Aliquid, tenetur?
-    </>
+    <div className="hero ">
+      <div className="relative">
+        <img
+          src="https://image.tmdb.org/t/p/original/rDYSuBYKR4US3A0XfuCiFzVq5CJ.jpg"
+          alt="hero-mage"
+        />
+      </div>
+      <div className="text-white absolute top-30 flex gap-20 w-full px-4 md:px-8">
+        <div className="w-[20%]">
+          <img
+            src="https://image.tmdb.org/t/p/original//oCoTgC3UyWGfyQ9thE10ulWR7bn.jpg"
+            alt="poster-image"
+            className="w-[240px] rounded-2xl"
+            loading="lazy"
+          />
+        </div>
+        <div className="movieContent w-[60%]">
+          <h2 className="font-bold text-[40px] tracking-[3px]"> Companion</h2>
+          <div className="smally flex gap-5 text-[16px]">
+            <p>2025</p>
+            <p className="flex items-center gap-1">
+              <span>
+                <PiClockCountdown className="text-[20px]" />
+              </span>
+
+              <p>97</p>
+            </p>
+            <p className="flex items-center gap-1">
+              <span>
+                <BsStarFill className="text-[20px]" />
+              </span>
+              7.10 /<span className="text-[15px] text-gray-600">10</span>
+            </p>
+            <p> Trailer</p>
+          </div>
+          <div className="flex gap-10">
+            <div className="border-1 border-white rounded-2xl px-2 py-[2px] text-center text-[16px]">
+              Horror
+            </div>
+            <div className="border-1 border-white rounded-2xl px-2 py-[2px] text-center text-[16px]">
+              Science Fiction
+            </div>
+            <div className="border-1 border-white rounded-2xl px-2 py-[2px] text-center text-[16px]">
+              Thriller
+            </div>
+          </div>
+          <p className="text-[13px] pr-4">
+            During a weekend getaway at a secluded lakeside estate, a group of
+            friends finds themselves entangled in a web of secrets, deception,
+            and advanced technology. As tensions rise and loyalties are tested,
+            they uncover unsettling truths about themselves and the world around
+            them.
+          </p>
+          <div className="flex gap-3 ">
+            <p>
+              <RiPlayFill />
+            </p>
+            <span> Watch Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
