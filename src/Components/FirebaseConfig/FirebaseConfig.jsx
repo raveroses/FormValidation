ce1ce0fb0113053b05f3d12f4d6ce08ccaad24e3
rave-roses <odekunlewaris01@gmail.com>
2025-06-29 14:03:42 +0100

    rechecking the signup

--- src/Components/FirebaseConfig/FirebaseConfig.jsx
+++ src/Components/FirebaseConfig/FirebaseConfig.jsx
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpihNcHkNuMDyZ81nw5c43dDyGRRozCKM",
-  authDomain: "form-validation-2931e.firebaseapp.com",
-  projectId: "form-validation-2931e",
-  storageBucket: "form-validation-2931e.firebasestorage.app",
-  messagingSenderId: "873866967498",
-  appId: "1:873866967498:web:dd87c4a2f8724e9b3eb733",
-  measurementId: "G-58PEP74SJM",
+  apiKey: "AIzaSyA1TMmLHGJSQkyO9nht5oG-9kKBg0LcHXY",
+  authDomain: "facebookclone-16efc.firebaseapp.com",
+  projectId: "facebookclone-16efc",
+  storageBucket: "facebookclone-16efc.firebasestorage.app",
+  messagingSenderId: "1077064481627",
+  appId: "1:1077064481627:web:e8b7448e762220f3cc787e",
+  measurementId: "G-R2427KW7MM",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
